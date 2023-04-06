75582c6b52caf2a913b7a420c4fd2d0c89ea4724
mybenim <agro-inna@mail.ru>
2023-04-06 23:47:52 +0500

    js

--- scripts/index.js
+++ scripts/index.js
@@ -3,29 +3,28 @@ const popupCloseButtonElement = popupElement.querySelector(".popup__close");
 const popupOpenButtonElement = document.querySelector(".profile__square");
 
 const togglePopupVisibility = function () {
-    popupElement.classList.toggle("popup_opened");
+    popupElement.classList.add("popup__opened");
+    nameInput.textContent = nameInput.value="";
+    jobInput.textContent = jobInput.value="";
+    popupTitleElement.value = popupTitleElement.textContent;
+    popupJobElement.value = popupJobElement.textContent;
 };
 popupOpenButtonElement.addEventListener("click", togglePopupVisibility);
 
-const closePopupVisibility = function () {
-    popupElement.classList.remove("popup_opened");
-};
+function closePopupVisibility() {
+  popupElement.classList.remove("popup__opened");
+}
 popupCloseButtonElement.addEventListener("click", closePopupVisibility);
 
-const nameInput = popupElement.querySelector(".popup__input-container");
-const jobInput = popupElement.querySelector(".popup__input-container");
+const nameInput = popupElement.querySelector(".popup__input-container_ctrl_fullname");
+const jobInput = popupElement.querySelector(".popup__input-container_ctrl_job");
 const popupTitleElement = document.querySelector(".profile__title");
 const popupJobElement = document.querySelector(".profile__subtitle");
 
 const formElement = popupElement.querySelector(".popup__form");
 formElement.addEventListener("submit", handleFormSubmit);
 
-
 function handleFormSubmit(event) {
     event.preventDefault();
-    nameInput.textContent = popupElement.querySelector(".popup__input-container").value;
-    jobInput.textContent = popupElement.querySelector(".popup__input-container").value;
-    popupTitleElement.value = document.querySelector(".profile__title").textContent;
-    popupJobElement.value = document.querySelector(".profile__subtitle").textContent;
+    closePopup();
 };
-     /* :(  */
