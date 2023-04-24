751e9b044cba3eda373333c71ede518945b44fce
mybenim <agro-inna@mail.ru>
2023-04-25 01:29:41 +0500

    отправить на проверку

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 const popupElement = document.querySelector(".popup");
 const popupCloseButtonElement = popupElement.querySelector(".popup__close");
 const popupOpenButtonElement = document.querySelector(".profile__square");
+const popupSafeElement = popupElement.querySelector(".popup__safe")
+const nameInput = popupElement.querySelector(".popup__input-container_ctrl_fullname");
+const jobInput = popupElement.querySelector(".popup__input-container_ctrl_job");
+const popupTitleElement = document.querySelector(".profile__title");
+const popupJobElement = document.querySelector(".profile__subtitle");
 
 const togglePopupVisibility = function () {
     popupElement.classList.add("popup__opened");
@@ -9,13 +14,12 @@ const togglePopupVisibility = function () {
 };
 popupOpenButtonElement.addEventListener("click", togglePopupVisibility);
 
-const nameInput = popupElement.querySelector(".popup__input-container_ctrl_fullname");
-const jobInput = popupElement.querySelector(".popup__input-container_ctrl_job");
-const popupTitleElement = document.querySelector(".profile__title");
-const popupJobElement = document.querySelector(".profile__subtitle");
-
 const formElement = popupElement.querySelector(".popup__form");
 formElement.addEventListener("submit", handleFormSubmit);
+
+popupSafeElement.addEventListener("click", () => {
+  popupElement.classList.remove("popup__opened");
+});
 
 function handleFormSubmit(event) {
     event.preventDefault();
