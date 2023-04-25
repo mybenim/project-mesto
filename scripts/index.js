9dd6f6ab06cf40f12f233408d5cf6273af2f16dd
mybenim <agro-inna@mail.ru>
2023-04-26 01:53:50 +0500

    исправила

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,13 @@
 const popupElement = document.querySelector(".popup");
 const popupCloseButtonElement = popupElement.querySelector(".popup__close");
 const popupOpenButtonElement = document.querySelector(".profile__square");
-const popupSafeElement = popupElement.querySelector(".popup__safe")
 const nameInput = popupElement.querySelector(".popup__input-container_ctrl_fullname");
 const jobInput = popupElement.querySelector(".popup__input-container_ctrl_job");
 const popupTitleElement = document.querySelector(".profile__title");
 const popupJobElement = document.querySelector(".profile__subtitle");
 
 const togglePopupVisibility = function () {
-    popupElement.classList.add("popup__opened");
+    popupElement.classList.add("popup_opened");
     nameInput.value = popupTitleElement.textContent;
     jobInput.value = popupJobElement.textContent;
 };
@@ -17,17 +16,21 @@ popupOpenButtonElement.addEventListener("click", togglePopupVisibility);
 const formElement = popupElement.querySelector(".popup__form");
 formElement.addEventListener("submit", handleFormSubmit);
 
-popupSafeElement.addEventListener("click", () => {
-  popupElement.classList.remove("popup__opened");
-});
-
 function handleFormSubmit(event) {
     event.preventDefault();
     popupTitleElement.textContent = nameInput.value;
     popupJobElement.textContent = jobInput.value;
+
+    const closePopupVisibility = function () {
+        popupElement.classList.remove("popup_opened");
+    };
+    popupCloseButtonElement.addEventListener("click", closePopupVisibility);
+
+    const popupSafeElement = function () {
+        popupElement.classList.remove("popup_opened");
+    };
+    popupElement.addEventListener("click", popupSafeElement);
 };
 
-function closePopupVisibility() {
-  popupElement.classList.remove("popup__opened");
-}
-popupCloseButtonElement.addEventListener("click", closePopupVisibility);
+
+
